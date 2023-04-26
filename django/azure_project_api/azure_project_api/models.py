17f079523bca2832c561f073b16911c3028ac669
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2023-04-26 16:33:56 +0200

    conexion django et azur

--- django/azure_project_api/azure_project_api/models.py
+++ django/azure_project_api/azure_project_api/models.py
@@ -1,23 +1,25 @@
 from djongo import models
+from django.contrib import admin
+
+class Format(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length= 50, null=False, blank=False)
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length= 50, null=False, blank=False)
 
 class Photo(models.Model):
-    _id = models.ObjectIdField()
+    id = models.AutoField(primary_key=True)
     name = models.TextField(max_length= 50, null=False, blank=False)
     picture = models.TextField(max_length= 255, null=False, blank=False)
     description = models.TextField(max_length= 255, null=False, blank=False)
     target_client = models.TextField(max_length= 255, null=False, blank=False)
-    logo = models.BooleanField()
-    pictures_human = models.BooleanField()
-
-class Format(models.Model):
-    _id = models.ObjectIdField()
-    name = models.TextField(max_length= 50, null=False, blank=False)
-
-class Tag(models.Model):
-    _id = models.ObjectIdField()
-    name = models.TextField(max_length= 50, null=False, blank=False)
+    logo = models.BooleanField(default=False)
+    pictures_human = models.BooleanField(default=False)
+    format_id = models.ForeignKey(Format, on_delete=models.CASCADE)
 
 class Photos_Tags(models.Model):
-    _id_photo = models.ObjectIdField()
-    _id_tag = models.ObjectIdField()
+    photo_id = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
--- django/azure_project_api/azure_project_api/settings.py
+++ django/azure_project_api/azure_project_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "azure_project_api",
     'rest_framework',
 ]
 
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'azure_project_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "mssql",
+        "NAME": "Pomona",
+        "USER": "kevinl45",
+        "PASSWORD": "kasthorimie45:",
+        "HOST": "pomona-projet.database.windows.net",
+        "PORT": "1433",
+    },
 }
 
 
