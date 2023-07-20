112b8aa19a6e92358ca15b30c39f8e23ccb490cd
cy <zkglbq7o4yd9@opayq.net>
2023-07-20 15:27:24 +0200

    Added environment, models and somethin' i forgot

--- angular/image-search/src/app/app-routing.module.ts
+++ angular/image-search/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ImageSearchComponent} from "./components/image-search/image-search.component";
-import {ImageUploadComponent} from "./components/image-upload/image-upload.component";
 import { FormPictureUploadComponent } from './components/form-picture-upload/form-picture-upload.component';
 import { HomeComponent} from "./components/home/home.component";
+import { ImageModelComponent } from "./components/image-model/image-model.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search', component: ImageSearchComponent },
   // { path: 'upload', component: ImageUploadComponent },
   { path: 'upload', component: FormPictureUploadComponent },
+  { path: 'image_detail', component: ImageModelComponent}
   // other routes...
 ];
 
---
+++ angular/image-search/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  API_URL:"http://127.0.0.1:8000/",
+  PHOTO_URL:"blobs/pictures/paths/",
+  UPLOAD_URL:"upload/pictures/",
+  CPU_VIS:"computer-vision/analyze/"
+}
